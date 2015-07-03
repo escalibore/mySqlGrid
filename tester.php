71017442e33653f831c74d9d53d92a57b2a3409e
escalibore <tom@tberthold.com>
2015-07-03 00:21:32 -0500

    update

--- tester.php
+++ tester.php
@@ -1,5 +1,5 @@
 <?php
-    $mysqli = new mysqli('localhost', 'xxxx', 'xxxx','xxxxx');
+    $mysqli = new mysqli('localhost', 'tberthol_chinook', 'password','tberthol_chinook');
     if($mysqli->connect_error) {
         die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);    
     }
@@ -10,13 +10,10 @@
         LEFT JOIN Artist a ON a.ArtistId = al.ArtistId
         LEFT JOIN Genre g ON g.GenreId = t.GenreId",
         'connection' => $mysqli,
-        'lineCount' => 20
     );
     include 'mysqlgrid/mysqlgrid.php';
 ?>
-<body style='margin:0.5%;'>
-<div style='text-align:center;'><h2>Demo of <span style='color:#337AB7;'>MySqlGrid</span></h2></div>
-<div id="mySqlGridTable" style='text-align: center;'></div>
-<div id="mySqlGridPagination" style='text-align: center;'></div>
-<div style="text-align: center;"><a href="https://github.com/escalibore/mySqlGrid">Download MySqlGrid from GitHub</a></div>
-</body> 
+<body>
+<div id="mySqlGridTable"></div>
+<div id="mySqlGridPagination"></div>
+</body>
