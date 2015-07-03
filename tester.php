797f5e3cdc9453414e406d40bbfa83020a4df262
escalibore <tom@tberthold.com>
2015-07-03 02:25:25 -0500

    update

--- tester.php
+++ tester.php
@@ -1,8 +1,6 @@
 <?php
     $mysqli = new mysqli('localhost', 'tberthol_chinook', 'password','tberthol_chinook');
-    if($mysqli->connect_error) {
-        die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);    
-    }
+    if($mysqli->connect_error) die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
     $mySqlGridArray = array(
         'sql' => "
         SELECT t.Name Track, al.Title Album, a.Name Artist, Composer, g.Name Genre FROM Track t
@@ -14,6 +12,6 @@
     include 'mysqlgrid/mysqlgrid.php';
 ?>
 <body>
-<div id="mySqlGridTable"></div>
-<div id="mySqlGridPagination"></div>
+    <div id="mySqlGridTable"></div>
+    <div id="mySqlGridPagination"></div>
 </body>
