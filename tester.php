6d472a1fcfc2f4e0755c9572663ee1d816e62126
escalibore <tom@tberthold.com>
2015-07-04 01:24:44 -0500

    update

--- tester.php
+++ tester.php
@@ -1,17 +1,26 @@
 <?php
-    $mysqli = new mysqli('localhost', 'tberthol_chinook', 'password','tberthol_chinook');
-    if($mysqli->connect_error) die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
-    $mySqlGridArray = array(
+    $mySqlGridOptions = array(
         'sql' => "
         SELECT t.Name Track, al.Title Album, a.Name Artist, Composer, g.Name Genre FROM Track t
         LEFT JOIN Album al ON al.AlbumId = t.AlbumId
         LEFT JOIN Artist a ON a.ArtistId = al.ArtistId
         LEFT JOIN Genre g ON g.GenreId = t.GenreId",
-        'connection' => $mysqli,
+        'lineCount' => 20,
+//       'hideColumns' => array('Artist'),
+        //   'includePath' => '../thecosmicsage/mysqlgrid'
     );
     include 'mysqlgrid/mysqlgrid.php';
+    //   include '../thecosmicsage/mysqlgrid/mysqlgrid.php';
 ?>
-<body>
-    <div id="mySqlGridTable"></div>
-    <div id="mySqlGridPagination"></div>
+<body style='margin:0.5%;'><br>
+    <div style='text-align:center;'><h2>Demo of <span style='color:#337AB7;'>MySqlGrid</span></h2></div>
+    <div id="mySqlGridTable" style='text-align: center;'></div>
+    <div id="mySqlGridPagination" style='text-align: center;'></div>
+    <div style="text-align: center;"><a href="https://github.com/escalibore/mySqlGrid">Download MySqlGrid from GitHub</a></div>
 </body>
+
+
+
+
+
+
