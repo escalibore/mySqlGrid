31d0c5e64396a9f147ecf942dee8ebf383afea35
escalibore <tom@tberthold.com>
2015-12-26 22:25:03 -0600

    add notice

--- src/dbconnect.php
+++ src/dbconnect.php
@@ -1,6 +1,8 @@
 <?php
 // Add mysqli connection details below...
 
+echo "You have not entered database connection details...<br>";  // Remove this after entering db connection details.
+
 //Sample:
 //$mySqlGridConnection = new mysqli('localhost', 'UserName', 'Password', 'Database');
 //if($mySqlGridConnection->connect_error) die('Connect Error (' . $mySqlGridConnection->connect_errno . ') '. $mySqlGridConnection->connect_error);
