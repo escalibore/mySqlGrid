39cc39ea3971651b6d310f04d1ec423717acda1d
escalibore <tom@tberthold.com>
2016-01-02 11:21:53 -0600

    Update dbconnect.php

--- src/dbconnect.php
+++ src/dbconnect.php
@@ -7,6 +7,6 @@ echo "You have not entered database connection details...<br>";  // Remove this 
 //$mySqlGridConnection = new mysqli('localhost', 'UserName', 'Password', 'Database');
 //if($mySqlGridConnection->connect_error) die('Connect Error (' . $mySqlGridConnection->connect_errno . ') '. $mySqlGridConnection->connect_error);
 
-//Below can be used if you need to specify the database dynamically.  In this case you would provide option: "database" in $mySqlGridOptions.
+//Below can be used if you need to specify the database dynamically.  In this case you would include parameter: "database".
 //if($optionsArray['database']) mysqli_select_db($mySqlGridConnection, $optionsArray['database']);
 ?>
